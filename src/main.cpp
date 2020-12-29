cee23ae9857cbacb0863129607da6daea6eec155
Ryzerth <whatsthetgeek@gmail.com>
2020-12-29 21:06:21 +0100

    switched test code

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
 #include <iostream>
 #include <random>
 #include <spdlog/spdlog.h>
+#include <wav.h>
 
 #include <dsp/convertion.h>
 #include <dsp/processing.h>
 #include <dsp/resampling.h>
+#include <dsp/filter.h>
 #include <dsp/sink.h>
 
-#define SAMPLE_COUNT    1000000
+#define SAMPLE_COUNT    (5 * 6000)
 #define RUN_COUNT       100
 
 int main() {
@@ -27,33 +29,64 @@ int main() {
 
     // ======= DUT =======
     dsp::stream<float> inputStream;
-    dsp::RealToComplex r2c(&inputStream);
-    dsp::FrequencyXlator<dsp::complex_t> xlator(&r2c.out, 64000000, 14000000);
-    dsp::PowerDecimator decim(&xlator.out, 3);
-    dsp::NullSink<dsp::complex_t> ns(&decim.out);
-    r2c.start();
-    xlator.start();
-    decim.start();
-    ns.start();
+
+    dsp::filter_window::BlackmanWindow win(1000, 100, 6000);
+    dsp::FIR<float> resamp(&inputStream, &win);//, 6000, 48000);
+    win.setSampleRate(6000);// * resamp.getInterpolation());
+    resamp.updateWindow(&win);
+
+    resamp.start();
+    
+    WavWriter wavWriter("output.wav", 16, 1, 6000);
     // ===================
 
-    double totalWait = 0.0f;
 
-    for (int i = 0; i < RUN_COUNT; i++) {
-        auto start = std::chrono::high_resolution_clock::now();
+    // Write to input
+    memcpy(inputStream.writeBuf, randBuf, SAMPLE_COUNT * sizeof(float));
+    inputStream.swap(SAMPLE_COUNT);
 
-        memcpy(inputStream.writeBuf, randBuf, SAMPLE_COUNT * sizeof(float));
-        inputStream.swap(SAMPLE_COUNT);
+    // Read from output
+    int count = resamp.out.read();
+    int16_t* samples = new int16_t[count];
+    for (int i = 0; i < count; i++) {
+        samples[i] = resamp.out.readBuf[i] * 10000.0f;
+    }
+    wavWriter.writeSamples(samples, count * sizeof(int16_t));
+    wavWriter.close();
 
-        auto stop = std::chrono::high_resolution_clock::now();
+    
 
-        auto delay = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        totalWait += delay.count();
+    count = win.getTapCount();
+    float* taps = new float[count];
+    win.createTaps(taps, count);
+    for (int i = 0; i < count; i++) {
+        printf("%f\n", taps[i]);
     }
 
-    double avgWait = totalWait / (double)RUN_COUNT;
 
-    spdlog::info("Average run took {0} uS ({1} MS/s)", avgWait, 1000000.0 / avgWait);
+
+
+
+
+
+
+    // double totalWait = 0.0f;
+
+    // for (int i = 0; i < RUN_COUNT; i++) {
+    //     auto start = std::chrono::high_resolution_clock::now();
+
+    //     memcpy(inputStream.writeBuf, randBuf, SAMPLE_COUNT * sizeof(float));
+    //     inputStream.swap(SAMPLE_COUNT);
+
+    //     auto stop = std::chrono::high_resolution_clock::now();
+
+    //     auto delay = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    //     totalWait += delay.count();
+    // }
+
+    // double avgWait = totalWait / (double)RUN_COUNT;
+
+    // spdlog::info("Average run took {0} uS ({1} MS/s)", avgWait, 1000000.0 / avgWait);
 
     return 0;
 }
